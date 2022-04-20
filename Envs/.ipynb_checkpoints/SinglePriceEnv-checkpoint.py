ff999c8d0f0caf319d3056b86120bcaaef9445bb
EC2 Default User <ec2-user@ip-172-16-30-231.us-west-2.compute.internal>
2022-04-20 20:47:32 +0000

    update

--- Envs/.ipynb_checkpoints/SinglePriceEnv-checkpoint.py
+++ Envs/.ipynb_checkpoints/SinglePriceEnv-checkpoint.py
@@ -35,7 +35,7 @@ class PriceEnv(gym.Env):
         self.t = 0
         
         ## new start here
-        
+        self.action_space = gym.spaces.
     
     def reset(self):
         self.state = np.repeat(0, 2*self.T)
